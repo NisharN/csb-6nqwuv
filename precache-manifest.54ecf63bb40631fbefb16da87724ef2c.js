ae8e6c4ba725aee2a9ea312ba33c8cc10b905693
Code Sandbox <hello@codesandbox.io>
2022-12-10 13:13:38 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.54ecf63bb40631fbefb16da87724ef2c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847219775dfb57e6589bb62e532580d5",
+    "url": "https://nisharn.github.io/csb-6nqwuv/index.html"
+  },
+  {
+    "revision": "98d7eeff9e0def9c6339",
+    "url": "https://nisharn.github.io/csb-6nqwuv/static/js/2.2e4eed59.chunk.js"
+  },
+  {
+    "revision": "1689c12bd6a0cce5941d",
+    "url": "https://nisharn.github.io/csb-6nqwuv/static/js/main.5e4b2358.chunk.js"
+  },
+  {
+    "revision": "98602b15f0f0846d19a9",
+    "url": "https://nisharn.github.io/csb-6nqwuv/static/js/runtime-main.b12f6e32.js"
+  }
+]);
